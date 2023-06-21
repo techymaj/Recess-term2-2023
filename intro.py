7641bad31bbcb16c1c96113ef87f03b30cd5d4e4
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2023-06-21 12:13:02 +0300

    Renamed wilfred_majaliwa.py to intro.py

---
+++ intro.py
@@ -0,0 +1,58 @@
+# Running python scripts
+
+# 1. Open terminal
+# 2. Navigate to the folder containing the script
+# 3. Type python3 wilfred_majaliwa.py
+
+# Cooments in pythoon
+# Single line comment
+"""
+Multi line comment
+More 
+And More
+"""
+
+# Variables
+# A variable is a container for storing data values
+
+name = "Wilfred Majaliwa"
+recess = 2
+
+# Print out the variables
+print("Your name is "
+      + name
+      + " and you are doing your "
+      + str(recess) + "nd recess")
+
+# Data types
+# Strings
+# Strings are single or double quotes
+
+# Booleans - True or False
+# Sequence types
+# Lists - Mutable - Can be changed. Use square brackets
+# Tuples - Immutable - Cannot be changed. Use round brackets
+# range() - Immutable - Cannot be changed. Use round brackets
+# Sets - Mutable - Can be changed. Use curly brackets
+
+# Mapping types
+# Dictionaries - Mutable - Can be changed. Use curly brackets
+
+{
+    "name": "Wilfred Majaliwa",
+    "age": 29,
+}
+
+
+# Set types
+# Unique unorded collection of items
+
+# None types
+# None is a special data type in Python that represents the absence of a value or a null value
+gender = True
+gender = None
+
+print("Gender is: " + str(gender))
+
+# jeff.geoff.cis@gmail.com
+
